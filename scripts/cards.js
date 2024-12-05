1ed7f8bb9b262884ea0b968ebf634f3d1b1af09e
Oleg <Oleg@DESKTOP-TERFJCE>
2024-12-05 19:27:24 +0300

    Initial commit

---
+++ scripts/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://my-server/cards-nameproject/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://my-server/cards-nameproject/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://my-server/cards-nameproject/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://my-server/cards-nameproject/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://my-server/cards-nameproject/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://my-server/cards-nameproject/baikal.jpg",
+    }
+];
